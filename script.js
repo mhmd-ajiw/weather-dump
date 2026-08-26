14083f92995fd7899a3a0b197af0d530096cd6d6
mhmd-ajiw <journeyofaji@gmail.com>
2026-08-26 21:03:44 +0700

    progress 1

---
+++ script.js
@@ -0,0 +1,7 @@
+// key : af7748e9c898589c1b90c5ae09f95847
+const inputCity = document.getElementById('city-input');
+const btnSearch = document.getElementById('search-btn');
+const result = document.getElementById('weather-result');
+const nameCity = document.getElementById('city-name');
+const temp = document.getElementById('temperature');
+const descrip = document.getElementById('description');
